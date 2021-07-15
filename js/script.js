1d7566111cc7148fe30f12a7ead8611e5c781734
Wyndace (Васильев Александр) <wyndace@hotmail.com>
2021-07-15 18:22:23 +0500

    added slider block

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 "use sctrict";
+
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+  },
+
+});
+
